958f6256ddd32b3293d626d98d4f6e0a88e4c504
Ryan <yangran@kingfar.cn>
2021-04-23 16:33:23 +0800

    升级flutter 1.22.6

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.headline4,
             ),
             Card(
               margin: const EdgeInsets.all(24.0),
